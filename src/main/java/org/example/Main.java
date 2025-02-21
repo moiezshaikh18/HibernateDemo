b1b0f522d003f61db6e47df7f920477ce331e65c
moiezshaikh18 <moiezshaikh18@gmail.com>
2025-02-21 21:47:55 +0530

    Hibernate Changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection("","","");
     }
 }
